7a05712aa74df2435843e6f3cb59084e6f4db955
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-08-16 20:02:25 +0200

    adds svg logo and browser icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from '/logo.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  //https://www.youtube.com/watch?v=fZPgBnL2x-Q <- sustand tut
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+      <img src={logo} className="logo" alt="logo" />
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
