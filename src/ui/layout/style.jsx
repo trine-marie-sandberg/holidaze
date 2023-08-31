2a4f82d33aa6afa54c8c998d87148d5da90d06d6
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-08-31 14:20:49 +0200

    adjust background gradient

--- src/ui/layout/style.jsx
+++ src/ui/layout/style.jsx
@@ -8,7 +8,8 @@ export const Main = styled.main`
 
 export const Wrapper = styled.div`
   background-color: var(--color-primary);
-  background-image: linear-gradient(rgb(6, 44, 67) 60%, rgb(53, 125, 136));
+  /* background-image: linear-gradient(rgb(6, 44, 67) 60%, rgb(53, 125, 136)); */
+  background-image: linear-gradient(rgb(4, 32, 51), rgb(6, 44, 67), rgb(53, 125, 136));
   display: grid;
   grid-template-rows: 1fr 7.5fr 0.5fr;
   height: 100vh;
