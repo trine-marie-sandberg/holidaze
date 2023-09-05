65e612c7b56436119a3528eee5e45ca38f53532b
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-09-05 15:24:13 +0200

    changed const from rating to initialRating

--- src/ui/star-rating/index.jsx
+++ src/ui/star-rating/index.jsx
@@ -4,29 +4,29 @@ import { RatingStars, LabelStar, InputStar, RatingWrap } from "./style";
 export default function StarRating(props) {
 
     const [ isChecked, setChecked ] = useState(false);
-    const [ rating, setRating ] = useState(0);
-    const r = props.children;
-    useEffect(() => r(rating), []);
+    const initialRating = 0;
+    const updateRating = props.children;
+    useEffect(() => updateRating(initialRating), []);
     return(
         <RatingWrap>
             <RatingStars>
                 <LabelStar htmlFor="rs0">
-                    <InputStar type="radio" name="rating" id="rs0" onChange={() => r(() => 0 +1)} />
+                    <InputStar type="radio" name="rating" id="rs0" onChange={() => updateRating(() => 0 +1)} />
                 </LabelStar>
                 <LabelStar htmlFor="rs1">
-                    <InputStar type="radio" name="rating" id="rs1" onChange={() => r(() => 0 +2)} />
+                    <InputStar type="radio" name="rating" id="rs1" onChange={() => updateRating(() => 0 +2)} />
                 </LabelStar>
                 <LabelStar htmlFor="rs2">
-                    <InputStar type="radio" name="rating" id="rs2" onChange={() => r(() => 0 +3)} />
+                    <InputStar type="radio" name="rating" id="rs2" onChange={() => updateRating(() => 0 +3)} />
                 </LabelStar>
                 <LabelStar htmlFor="rs3">
-                    <InputStar type="radio" name="rating" id="rs3" onChange={() => r(() => 0 +4)} />
+                    <InputStar type="radio" name="rating" id="rs3" onChange={() => updateRating(() => 0 +4)} />
                 </LabelStar>
                 <LabelStar htmlFor="rs4">
-                    <InputStar type="radio" name="rating" id="rs4" onChange={() => r(() => 0 +5)} />
+                    <InputStar type="radio" name="rating" id="rs4" onChange={() => updateRating(() => 0 +5)} />
                 </LabelStar>
                 <LabelStar htmlFor="rs5">
-                    <InputStar type="radio" name="rating" id="rs5" onChange={() => r(() => 0 +6)} />
+                    <InputStar type="radio" name="rating" id="rs5" onChange={() => updateRating(() => 0 +6)} />
                 </LabelStar>
             </RatingStars>
         </RatingWrap>
