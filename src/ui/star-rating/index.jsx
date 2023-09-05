00cd71402ca35f959e9d5d172c50b229177203d1
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-09-05 15:31:19 +0200

    simplify updateRating on inputs

--- src/ui/star-rating/index.jsx
+++ src/ui/star-rating/index.jsx
@@ -10,22 +10,22 @@ export default function StarRating(props) {
         <RatingWrap>
             <RatingStars>
                 <LabelStar htmlFor="rs0">
-                    <InputStar type="radio" name="rating" id="rs0" onChange={() => updateRating(() => 0 +1)} />
+                    <InputStar type="radio" name="rating" id="rs0" onChange={() => updateRating(() => 1)} />
                 </LabelStar>
                 <LabelStar htmlFor="rs1">
-                    <InputStar type="radio" name="rating" id="rs1" onChange={() => updateRating(() => 0 +2)} />
+                    <InputStar type="radio" name="rating" id="rs1" onChange={() => updateRating(() => 2)} />
                 </LabelStar>
                 <LabelStar htmlFor="rs2">
-                    <InputStar type="radio" name="rating" id="rs2" onChange={() => updateRating(() => 0 +3)} />
+                    <InputStar type="radio" name="rating" id="rs2" onChange={() => updateRating(() => 3)} />
                 </LabelStar>
                 <LabelStar htmlFor="rs3">
-                    <InputStar type="radio" name="rating" id="rs3" onChange={() => updateRating(() => 0 +4)} />
+                    <InputStar type="radio" name="rating" id="rs3" onChange={() => updateRating(() => 4)} />
                 </LabelStar>
                 <LabelStar htmlFor="rs4">
-                    <InputStar type="radio" name="rating" id="rs4" onChange={() => updateRating(() => 0 +5)} />
+                    <InputStar type="radio" name="rating" id="rs4" onChange={() => updateRating(() => 5)} />
                 </LabelStar>
                 <LabelStar htmlFor="rs5">
-                    <InputStar type="radio" name="rating" id="rs5" onChange={() => updateRating(() => 0 +6)} />
+                    <InputStar type="radio" name="rating" id="rs5" onChange={() => updateRating(() => 6)} />
                 </LabelStar>
             </RatingStars>
         </RatingWrap>
