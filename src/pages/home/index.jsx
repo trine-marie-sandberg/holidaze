76c21731ca0b71b33a4de16fe4dd7d8aea0b2e20
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-09-06 15:30:11 +0200

    adds search

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,29 @@
-import { BgFade, BgImg, FrontPage, SearchField, I, HideLabel, FrontPageWrap, SearchWrap } from "./style";
+import { BgFade, BgImg, FrontPage, SearchField, 
+         I, HideLabel, FrontPageWrap, SearchWrap, 
+         ResultsBox, ResultWrap, Wrap, StarsPositionWrap, 
+         HeadingCards, Position
+        } from "./style";
 import { Link } from "react-router-dom";
 import { BtnPrimary, LinkWrap } from "../../ui/btns/style";
+import useFetch from "../../hooks/api";
+import base, { page, created } from "../../constants.js";
+import { useState } from "react";
+import VenueCards from "../../ui/venue-cards";
 
 export default function HomePage() {
 
+    const url = `${base + created + page + 10}`;
+    const { data, loading, error } = useFetch(url);
+    const [ filteredData, setFilteredData ] = useState(data);
+    const [ searchWord, setSearchWord ] = useState("");
+
+    function handleFilter() {
+        const newFilter = data.filter((value) => {
+             let searchName = value.name.toLowerCase().includes(searchWord.toLowerCase());
+             return searchName;
+          })
+          setFilteredData(newFilter);
+    }
     return(
         <BgImg>
             <FrontPage>
@@ -13,8 +33,38 @@ export default function HomePage() {
                         <SearchWrap>
                             <HideLabel htmlFor="search">Search </HideLabel>
                             <I className="fa-solid fa-search" aria-label="search icon"></I>
-                            <SearchField type="text" id="search" aria-label="search input field" placeholder="search for destination, contry, venue .."/>
+                            <SearchField 
+                              type="text" 
+                              id="search" 
+                              aria-label="search input field" 
+                              placeholder="search for destination, contry, venue .."
+                              onChange={(e) => {
+                                setSearchWord(e.target.value);
+                                handleFilter()
+                              }}
+                              />
                         </SearchWrap>
+                        <ResultsBox>
+                            {filteredData && 
+                              <ResultWrap>
+                                {filteredData.map((data) => {
+                                    try {
+                                        return(
+                                            <div key={data.id}>
+                                                <h2>{data.name}</h2>
+                                            </div>
+                                            
+                                        )
+                                    } catch(error) {
+                                        console.log(error)
+                                        return(
+                                            <p>{error}</p>
+                                        )
+                                    }
+                                    })}
+                               </ResultWrap> 
+                            }
+                        </ResultsBox>
                         <LinkWrap>
                             <Link to="venues">
                               <BtnPrimary>Explore</BtnPrimary>
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -53,3 +53,57 @@ export const SearchWrap = styled.form`
   display: flex;
   align-items: center;
 `;
+export const ResultsBox = styled.div`
+  overflow-y: scroll;
+  max-height: 47vh;
+  width: 100%;
+  position: absolute;
+  top: 45%;
+`;
+export const ResultWrap = styled.div`
+  background-color: ${(props) => props.theme.color.darker};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+
+//
+export const HeadingCards = styled.h2`
+  margin: 0;
+`;
+export const Image = styled.div`
+  width: 100%;
+  height: 200px;
+  background-size: 100% auto;
+  background-repeat: no-repeat;
+  max-height: ${(props) => props.theme.sizes.card};
+  border-radius: ${(props) => props.theme.sizes.borderRadius};
+`;
+export const Wrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-items: center;
+  padding: 0 ${(props) => props.theme.sizes.sm};
+`;
+export const Star = styled.div`
+  background-color: ${(props) => props.theme.color.font};
+  width: ${(props) => props.theme.sizes.sm};
+  height: ${(props) => props.theme.sizes.sm};
+  clip-path: polygon(50% 0%, 66% 32%, 100% 38%, 78% 64%, 83% 100%, 50% 83%, 17% 100%, 22% 64%, 0 38%, 34% 32%);
+`;
+export const Position = styled.p`
+  padding: ${(props) => props.theme.sizes.sm};
+  margin: 0;
+`;
+export const StarsPositionWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+export const Metas = styled.p`
+  margin: 0;
+`;
