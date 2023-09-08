13a5f931eb76a4cd36178489f4cd70823beb1817
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-09-08 11:18:02 +0200

    fix imports for calendar

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Theme from './styles/theme.jsx'
 import GlobalStyle from './styles/global.jsx'
-import './styles/theme.jsx'
-import 'react-date-range/dist/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/ui/calendar/index.jsx
+++ src/ui/calendar/index.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useRef, useState } from "react";
 import { Calendar } from "react-date-range";
+import format from "date-fns/format";
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 export default function DatePicker() {
+    const [ calendar, setCalendar ] = useState("");
+    const handleSelect = (date) => {
+        console.log(date)
+    }
+
     return(
         <div>
             <h2>Calendar</h2>
+            <div className="calendarWrap">
+                <Calendar
+                  date={new Date()}
+                  onChange={handleSelect}
+                  className="calendarElement"
+                />
+            </div>
         </div>
     )
 }
