dd8d7aef0e622f2152f99e717d7863ca66d1495e
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-09-09 00:30:19 +0200

    adds range functionality

--- src/ui/calendar/index.jsx
+++ src/ui/calendar/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react";
-import { Calendar } from "react-date-range";
+import { Calendar, DateRange, DateRangePicker } from "react-date-range";
+import { addDays } from "date-fns";
 import format from "date-fns/format";
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
@@ -7,18 +8,25 @@ import 'react-date-range/dist/theme/default.css';
 export default function DatePicker() {
 
     const [ calendar, setCalendar ] = useState("");
+    const [ range, setRange ] = useState([
+        {
+            startDate: new Date(),
+            endDate: addDays(new Date(), 7),
+            key: "selection",
+        }
+    ]);
     const [ open, setOpen ] = useState(false);
 
     useEffect(() => {
-        setCalendar(format(new Date(), "MM/dd/yyyy"));
+        // setCalendar(format(new Date(), "MM/dd/yyyy"));
         document.addEventListener("keydown", hideOnEscape, true);
         document.addEventListener("click", hidenOnClickOutside, true);
     }, []);
 
-    function handleSelect(date) {
-        console.log(date)
-        setCalendar(format(date, "MM/dd/yyyy"))
-    }
+    // function handleSelect(date) {
+    //     console.log(date)
+    //     setCalendar(format(date, "MM/dd/yyyy"))
+    // }
     function hideOnEscape(event) {
         if(event.key === "Escape") {
             setOpen(false);
@@ -36,16 +44,21 @@ export default function DatePicker() {
             <h2>Calendar</h2>
             <div className="calendarWrap">
                 <input 
-                  value={calendar} 
-                  readOnly
-                  className="inputBox"
-                  onClick={() => setOpen(open => !open)}
+                    value={`${format(range[0].startDate, "MM/dd/yyyy")} to ${format(range[0].endDate, "MM/dd/yyyy")}`} 
+                    readOnly
+                    className="inputBox"
+                    onClick={() => setOpen(open => !open)}
                 />
                 <div ref={refOne}>
                     {open &&
-                    <Calendar
+                    <DateRangePicker
                     date={new Date()}
-                    onChange={handleSelect}
+                    onChange={item => setRange([item.selection])}
+                    editableDateInputs= {true}
+                    moveRangeOnFirstSelection={false}
+                    ranges={range}
+                    months={2}
+                    direction="horizontal"
                     className="calendarElement"
                     />
                     }
