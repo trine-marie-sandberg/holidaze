e41bb15ff123c0b393055a850b86e16c3f5486fa
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-09-08 23:51:01 +0200

    Adds open/close to callendar

--- src/ui/calendar/index.jsx
+++ src/ui/calendar/index.jsx
@@ -5,21 +5,55 @@ import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 
 export default function DatePicker() {
+
     const [ calendar, setCalendar ] = useState("");
-    const handleSelect = (date) => {
+    const [ open, setOpen ] = useState(false);
+
+    useEffect(() => {
+        setCalendar(format(new Date(), "MM/dd/yyyy"));
+        document.addEventListener("keydown", hideOnEscape, true);
+        document.addEventListener("click", hidenOnClickOutside, true);
+    }, []);
+
+    function handleSelect(date) {
         console.log(date)
+        setCalendar(format(date, "MM/dd/yyyy"))
+    }
+    function hideOnEscape(event) {
+        if(event.key === "Escape") {
+            setOpen(false);
+        }
+    }
+    const refOne = useRef(null)
+    function hidenOnClickOutside(event) {
+        if(refOne.current && !refOne.current.contains(event.target)) {
+            setOpen(false);
+        }
     }
 
     return(
         <div>
             <h2>Calendar</h2>
             <div className="calendarWrap">
-                <Calendar
-                  date={new Date()}
-                  onChange={handleSelect}
-                  className="calendarElement"
+                <input 
+                  value={calendar} 
+                  readOnly
+                  className="inputBox"
+                  onClick={() => setOpen(open => !open)}
                 />
+                <div ref={refOne}>
+                    {open &&
+                    <Calendar
+                    date={new Date()}
+                    onChange={handleSelect}
+                    className="calendarElement"
+                    />
+                    }
+                </div>
             </div>
         </div>
     )
 }
+//https://hypeserver.github.io/react-date-range/
+//https://www.npmjs.com/package/react-date-range 
+//https://www.youtube.com/watch?v=5OEOLDBow_0
