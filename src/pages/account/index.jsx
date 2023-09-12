8c637ea59c5e357fbf9be6deba27f69fd3ea24e9
Trine Marie Sandberg <trine.marie.sandberg@gmail.com>
2023-09-12 17:13:23 +0200

    updates account page

--- src/pages/account/index.jsx
+++ src/pages/account/index.jsx
@@ -1,15 +1,41 @@
+import { Link } from "react-router-dom";
 import { useLoad } from "../../hooks/storage";
 import PageWrapper from "../../ui/pagewrapper";
-import { AccountContainer } from "./style";
+import { AccountContainer, BookingVenueContainer, BookingVenueWrap, FlexWrap, UpdateBtn, UpdateIcon } from "./style";
+import { useState } from "react";
 
 export default function AccountPage() {
     const user = useLoad("user");
-
+    const [ isManager, setIsManager ] = useState(user.manager);
     return(
         <PageWrapper>
             <AccountContainer>
-                <h1>Hi, {user.name}</h1>
-                <p>Here is your account page.</p>
+                <BookingVenueContainer>
+                    <BookingVenueWrap>
+                        <div>
+                            <h2>My bookings</h2>
+                            <div>
+                                <p>Currently no bookings</p>
+                            </div>
+                        </div>
+                    </BookingVenueWrap>
+                </BookingVenueContainer>
+                <div>
+                    <h1>Hi, {user.name}</h1>
+                    <p>You have currently no reservations.</p>
+                    <Link to="/venues">
+                        <p>Look for venues</p>
+                    </Link>
+                    <FlexWrap>
+                        <h2>My account information</h2>
+                        <UpdateBtn>Update <UpdateIcon className="fa-solid fa-pen-clip"></UpdateIcon></UpdateBtn>
+                    </FlexWrap>
+                    <ul>
+                        <li>Username: {user.name}</li>
+                        <li>Email: {user.email}</li>
+                        <li>Venue manager: {user.manager}</li>
+                    </ul>
+                </div>
             </AccountContainer>
         </PageWrapper>
     )
--- src/pages/account/style.js
+++ src/pages/account/style.js
@@ -1,8 +1,38 @@
 import styled from "styled-components";
 
 export const AccountContainer = styled.div`
-  background-color: ${(props) => props.theme.color.darker};
   padding: ${(props) => props.theme.sizes.sm};
-  border-radius: ${(props) => props.theme.sizes.borderRadius};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+export const BookingVenueWrap = styled.div`
+  min-height: 500px;
   border: 1px solid ${(props) => props.theme.color.border};
+  background-color: ${(props) => props.theme.color.darker};
+  border-radius: ${(props) => props.theme.sizes.borderRadius};
+  padding: ${(props) => props.theme.sizes.sm};
+`;
+export const BookingVenueContainer = styled.div`
+  min-width: 50%;
+`;
+export const FlexWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+export const UpdateBtn = styled.button`
+  cursor: pointer;
+  height: fit-content;
+  margin: 0 ${(props) => props.theme.sizes.med};
+  padding: 8px;
+  border: 1px solid ${(props) => props.theme.color.border};
+  background-color: ${(props) => props.theme.color.dark};
+  color: ${(props) => props.theme.color.font};
+  border-radius: ${(props) => props.theme.sizes.borderRadius};
+`;
+export const UpdateIcon = styled.i`
+  padding: 0 ${(props) => props.theme.sizes.xs};
+  border-bottom: 1px dashed ${(props) => props.theme.color.font};
 `;
